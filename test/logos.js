764e3941291542290dfb5b590693d340bc8f3c53
JP Richardson <jprichardson@gmail.com>
2018-02-15 01:00:20 -0600

    test/logos: support for SVG

--- test/logos.js
+++ test/logos.js
@@ -4,11 +4,12 @@ const fs = require('fs')
 const path = require('path')
 const assets = require('../')
 
-const getLogo = (name) => path.join(__dirname, '..', 'logos', name + '.png')
+const getLogoPNG = (name) => path.join(__dirname, '..', 'logos', name + '.png')
+const getLogoSVG = (name) => path.join(__dirname, '..', 'logos', name + '.svg')
 
 test('logos', (t) => {
   for (const asset of assets) {
-    t.true(fs.existsSync(getLogo(asset.name)), `${asset.name} logo exists`)
+    t.true(fs.existsSync(getLogoPNG(asset.name)) || fs.existsSync(getLogoSVG(asset.name)), `${asset.name} logo exists`)
   }
 
   t.end()
